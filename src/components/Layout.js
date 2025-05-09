5728cbb6b9fb6bcaa3cb6fb097e9a816c861e3ab
Aadya Sharma <aadya1773sharma@gmail.com>
2025-05-09 19:54:16 +0530

    kk

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,19 +1,17 @@
+// Layout.js
 import React from "react";
 import { Link, Outlet } from "react-router-dom";
 
-function Layout() {
+export default function Layout() {
   return (
     <div className="main-container">
-      <h1>Clothing Store</h1>
       <nav>
-        <Link to="/">Home</Link> |{" "}
-        <Link to="/women">Women</Link> |{" "}
-        <Link to="/men">Men</Link>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/women">Women</Link></li>
+        </ul>
       </nav>
-      <hr />
       <Outlet />
     </div>
   );
 }
-
-export default Layout;
